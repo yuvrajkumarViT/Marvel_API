01f5bfdccd163736cf87e505abf77f5f198a39db
unknown <yuvrajkumar013@gmail.com>
2022-10-13 17:05:28 +0530

    final

---
+++ package/dataframe_creation.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from hashlib import md5
+import pandas as pd
+import requests
+import json
+import hashlib
+
+def create_df(pub_k,nameStartsWith,hash):
+    url="https://gateway.marvel.com:443/v1/public/characters"
+    df=[]
+    headers={'Content-Type':'application/json'}
+    n=0
+    for i in range(4):
+        param={'apikey' : pub_k,
+            'nameStartsWith': 'sp',
+            'ts' : '2',
+            'hash' : hash, 
+            'offset': n,
+            'limit' : 100}
+        n+=100
+        response=requests.get(url,headers=headers,params=param,verify=False)
+        #response.raise_for_statusresult=response.json()
+        results=response.json()
+               
+        op=results
+        new_row = []
+        for a in op['data']['results']:
+    
+            new_row.append(a['name'])
+            new_row.append(a['events']['available'])
+            new_row.append(a['series']['available'])
+            new_row.append(a['stories']['available'])
+            new_row.append(a['comics']['available'])
+            new_row.append(a['id'])
+            df.append(new_row)
+            new_row=[]
+    df_main = pd.DataFrame(df,columns=['Character_Name','Events','Series','Stories','Comis','id'])
+    return df_main
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 13:58:27 2022
+
+@author: Yuvraj
+"""
+
+from setuptools import setup
+
+setup(
+      name="package",
+      version="0.1",
+      desription="Created by yuvraj",
+      author="Yuvraj",
+      packages=["package"],
+      install_requires=[]
+      
+      )
+
